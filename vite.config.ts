44cce05e9c9bf3af60ac3e1c7ffa181a23b66591
Zaid <madin1014@gmail.com>
2025-09-25 19:41:47 +0700

    feat: configure Vitest for testing environment and add vitest.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.ts",
-  },
+  base: "https://ramadhinzaid.github.io/github-repository-explorer/",
 });
